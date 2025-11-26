fbe747dce246a6d5223d7f68c7b73899d169871c
Youngho Kwon <dy-08@naver.com>
2025-11-26 14:14:32 +0900

    deploy android

--- App.js
+++ App.js
@@ -4,6 +4,9 @@ import DateTimePicker from '@react-native-community/datetimepicker';
 import uuid from 'react-native-uuid';
 import { useState } from 'react';
 import * as ImagePicker from 'expo-image-picker';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 export default function App() {
     const [text, setText] = useState('');
@@ -76,33 +79,38 @@ export default function App() {
                         <Text style={styles.maintxt}>Todo App</Text>
                         <Text style={styles.subtxt}>Check your todos</Text>
                     </View>
-                    <View>
-                        <Pressable>
-                            <Text>다크모드</Text>
+                    <View style={styles.featuresBox}>
+                        <Pressable onPress={() => setTogglePicker((prev) => !prev)}>
+                            <Text>{formatDate(date)}</Text>
                         </Pressable>
+                        <View style={styles.featurePhotos}>
+                            <Pressable onPress={getPhoto}>
+                                <MaterialIcons name='add-a-photo' size={24} color='black' />
+                            </Pressable>
+                            <Pressable onPress={getGallery}>
+                                <FontAwesome name='photo' size={24} color='black' />
+                            </Pressable>
+                        </View>
                     </View>
                 </View>
-                <View>
-                    <Pressable onPress={() => setTogglePicker((prev) => !prev)}>
-                        <Text>{formatDate(date)}</Text>
-                    </Pressable>
-                    {togglePicker && (
-                        <DateTimePicker
-                            value={date}
-                            mode='date'
-                            display={Platform.OS === 'ios' ? 'spinner' : 'default'}
-                            onChange={handleDate}
-                        />
-                    )}
+                <View style={styles.dateWrap}>
+                    <LinearGradient
+                        colors={['#E9DCDB', '#DFDDDD30']} // 시작색, 끝색
+                        start={{ x: 0, y: 0 }} // 왼쪽 위
+                        end={{ x: 1, y: 0 }} // 오른쪽 위 (약 70deg 느낌)
+                        style={styles.dateWrap}
+                    >
+                        {togglePicker && (
+                            <DateTimePicker
+                                value={date}
+                                mode='date'
+                                display={Platform.OS === 'ios' ? 'spinner' : 'default'}
+                                onChange={handleDate}
+                            />
+                        )}
+                    </LinearGradient>
                 </View>
-                <View>
-                    <Pressable onPress={getPhoto}>
-                        <Text>사진찍기</Text>
-                    </Pressable>
-                    <Pressable onPress={getGallery}>
-                        <Text>갤러리</Text>
-                    </Pressable>
-                </View>
+
                 <View style={styles.imagePreviewBox}>
                     {photo && <Image style={styles.image} source={{ uri: photo }} />}
                 </View>
@@ -153,9 +161,14 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
     },
     header: {
+        height: 60,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
+        marginBottom: 20,
+        position: 'relative',
+        left: 0,
+        top: 0,
     },
     maintxt: {
         fontSize: 28,
@@ -164,74 +177,30 @@ const styles = StyleSheet.create({
     subtxt: {
         fontSize: 18,
     },
-
-    // title: {
-    //     fontSize: 20,
-    //     fontWeight: 'bold',
-    // },
-    // inputWrap: {
-    //     flexDirection: 'row',
-    //     justifyContent: 'center',
-    //     alignItems: 'center',
-    //     gap: 10,
-    //     padding: 20,
-    //     marginTop: 10,
-    //     marginBottom: 10,
-    // },
-    // input: {
-    //     width: 200,
-    //     borderWidth: 0.5,
-    //     borderColor: '#2d2d2d',
-    //     borderRadius: 4,
-    //     paddingTop: 10,
-    //     paddingBottom: 10,
-    //     paddingLeft: 10,
-    // },
-    // addButton: {
-    //     backgroundColor: 'transparent',
-    //     fontSize: 16,
-    //     borderWidth: 0.5,
-    //     padding: 11,
-    //     borderRadius: 4,
-    //     color: 'white',
-    //     fontWeight: 'bold',
-    //     backgroundColor: 'green',
-    // },
-    // deleteButton: {
-    //     backgroundColor: 'transparent',
-    //     fontSize: 10,
-    //     borderWidth: 0.5,
-    //     padding: 11,
-    //     borderRadius: 4,
-    //     color: 'white',
-    //     fontWeight: 'bold',
-    //     backgroundColor: 'crimson',
-    //     textAlign: 'center',
-    // },
-    // contents: {
-    //     flexDirection: 'row',
-    //     marginBottom: 20,
-    //     gap: 10,
-    //     justifyContent: 'space-between',
-    //     alignItems: 'center',
-    //     marginTop: 10,
-    // },
-    // contentBox: {
-    //     flexDirection: 'row',
-    //     gap: 14,
-    // },
-    // imagePreviewBox: {
-    //     width: '380',
-    //     aspectRatio: '16/9',
-    // },
-    // smallImage: {
-    //     width: '40',
-    //     height: '40',
-    //     backgroundColor: 'blue',
-    // },
-    // image: {
-    //     width: '100%',
-    //     height: '100%',
-    //     objectFit: 'cover',
-    // },
+    dateWrap: {
+        width: '100%',
+        height: 'auto',
+        background: '#E9DCDB',
+        borderRadius: 8,
+        background: 'linear-gradient(70deg, rgba(233, 220, 219, 1) 0%, rgba(223, 221, 221, 1) 100%)',
+    },
+    featuresBox: {
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        gap: 4,
+        justifyContent: 'center',
+    },
+    featurePhotos: {
+        flexDirection: 'row',
+        gap: 10,
+    },
+    image: {
+        width: '100%',
+        height: '100%',
+        objectFit: 'cover',
+    },
+    imagePreviewBox: {
+        width: '96%',
+        aspectRatio: '3/2',
+    },
 });
